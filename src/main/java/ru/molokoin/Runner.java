399cd2504d283ca2893788b60d5795f9bf56ef99
esoe <igor_nikulin@mail.ru>
2022-12-22 09:19:08 +0300

    подготовлен шаблон проекта

---
+++ src/main/java/ru/molokoin/Runner.java
@@ -0,0 +1,7 @@
+package ru.molokoin;
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Запущен текстовый редактор: \" Editor \" ... ");
+    }
+    
+}
