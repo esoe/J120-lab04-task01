5b3609a1783b7b69887fbd56ffb4c1cf16b340d1
esoe <igor_nikulin@mail.ru>
2022-12-23 11:07:41 +0300

    йй

--- src/main/java/ru/molokoin/gui/SimpleEditorListener.java
+++ src/main/java/ru/molokoin/gui/SimpleEditorListener.java
@@ -1,11 +1,13 @@
 package ru.molokoin.gui;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
-// Импорт классов, которые используются при решении данной задачи.
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
